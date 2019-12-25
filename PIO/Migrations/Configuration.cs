fd6328a9620ebd68aa8016d6af7fdd74acc98803
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2019-12-25 20:09:38 +0100

    Added first migration (tables for ASP.NET Idenitity)

---
+++ PIO/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace PIO.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<PIO.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(PIO.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
