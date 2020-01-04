5cbfc21587c812f24677e3a46cf7f218f9a8b9f8
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2020-01-04 23:21:42 +0100

    Added some seed data

---
+++ PIO/Migrations/202001042141519_DatabaseSeeding1.cs
@@ -0,0 +1,42 @@
+﻿namespace PIO.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class DatabaseSeeding1 : DbMigration
+    {
+        public override void Up()
+        {
+            SeedCategories();
+            SeedQuestions();
+            SeedAnswers();
+        }
+        
+        public override void Down()
+        {
+        }
+
+        private void SeedCategories()
+        {
+            Sql("INSERT INTO Categories (Name) VALUES ('Математика')");
+            Sql("INSERT INTO Categories (Name, ParentCategory_Id) VALUES ('Геометрија', '1')");
+            Sql("INSERT INTO Categories (Name, ParentCategory_Id) VALUES ('Алгебра', '1')");
+            Sql("INSERT INTO Categories (Name, ParentCategory_Id) VALUES ('Калкулус', '1')");
+
+            Sql("INSERT INTO Categories (Name) VALUES ('Каде можам да најдам')");
+
+            Sql("INSERT INTO Categories (Name) VALUES ('Економија')");
+            Sql("INSERT INTO Categories (Name, ParentCategory_Id) VALUES ('Микроекономија', '6')");
+            Sql("INSERT INTO Categories (Name, ParentCategory_Id) VALUES ('Макроекономија', '6')");
+        }
+
+        private void SeedQuestions()
+        {
+        }
+
+        private void SeedAnswers()
+        {
+
+        }
+    }
+}
--- PIO/Migrations/Configuration.cs
+++ PIO/Migrations/Configuration.cs
@@ -1,5 +1,7 @@
-namespace PIO.Migrations
+﻿namespace PIO.Migrations
 {
+    using PIO.Models;
+    using PIO.Models.Domain;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,10 +16,94 @@ namespace PIO.Migrations
 
         protected override void Seed(PIO.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            SeedCategories(context);
+            SeedQuestions(context);
+            SeedAnswers(context);
+        }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
+        private void SeedCategories(ApplicationDbContext context)
+        {
+            var categories = context.Categories;
+            categories.AddOrUpdate(new Category() { Id = 1, Name = "Математика", ParentCategory = null });
+            categories.AddOrUpdate(new Category() { Id = 2, Name = "Економија", ParentCategory = null });
+            categories.AddOrUpdate(new Category() { Id = 3, Name = "Каде можам да најдам", ParentCategory = null });
+            context.SaveChanges();
+
+            var math = categories.First(c => c.Name == "Математика");
+            categories.AddOrUpdate(new Category() { Id = 4, Name = "Геометрија", ParentCategory = math });
+            categories.AddOrUpdate(new Category() { Id = 5, Name = "Алгебра", ParentCategory = math });
+
+            var econ = categories.First(c => c.Name == "Економија");
+            categories.AddOrUpdate(new Category() { Id = 6, Name = "Микроекономија", ParentCategory = econ });
+            categories.AddOrUpdate(new Category() { Id = 7, Name = "Макроекономија", ParentCategory = econ });
+
+            context.SaveChanges();
+        }
+
+        private void SeedQuestions(ApplicationDbContext context)
+        {
+            var kade = context.Categories.First(c => c.Name == "Каде можам да најдам");
+            var geo = context.Categories.First(c => c.Name == "Геометрија");
+            var alg = context.Categories.First(c => c.Name == "Алгебра");
+
+            var usr = context.Users.First();
+
+            var questions = context.Questions;
+            questions.AddOrUpdate(new Question()
+            {
+                Id = 1,
+                Title = "Каде можам да најдам шарени патики?",
+                Description = "Каде, о, каде?",
+                DateCreated = DateTime.Now,
+                DateLastModified = DateTime.Now,
+                Category = kade,
+                AskedBy = usr
+            });
+
+            questions.AddOrUpdate(new Question()
+            {
+                Id = 2,
+                Title = "Коку е 2+5?",
+                DateCreated = DateTime.Now,
+                DateLastModified = DateTime.Now,
+                Category = alg,
+                AskedBy = usr
+            });
+
+            questions.AddOrUpdate(new Question()
+            {
+                Id = 3,
+                Title = "Колку е 80*90?",
+                DateCreated = DateTime.Now,
+                DateLastModified = DateTime.Now,
+                Category = alg,
+                AskedBy = usr
+            });
+
+            questions.AddOrUpdate(new Question()
+            {
+                Id = 4,
+                Title = "Како да најдам пресметам на додекахедрон?",
+                DateCreated = DateTime.Now,
+                DateLastModified = DateTime.Now,
+                Category = geo,
+                AskedBy = usr
+            });
+
+            questions.AddOrUpdate(new Question()
+            {
+                Id = 5,
+                Title = "Како да квадрирам круг?",
+                DateCreated = DateTime.Now,
+                DateLastModified = DateTime.Now,
+                Category = geo,
+                AskedBy = usr
+            });
+        }
+
+        private void SeedAnswers(ApplicationDbContext context)
+        {
+
         }
     }
 }
