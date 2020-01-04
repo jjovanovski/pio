bead51845f1f09de4496922efe5ed822cef75e4d
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2020-01-05 00:00:03 +0100

    Added basic side bar to the layout that shows the category tree loaded from db

---
+++ PIO/App_Start/PassCategoryTree.cs
@@ -0,0 +1,27 @@
+﻿using PIO.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PIO.App_Start
+{
+    public class PassCategoryTree : ActionFilterAttribute
+    {
+        private ICategoryRepository _categoryRepository;
+
+        public PassCategoryTree()
+        {
+            _categoryRepository = new CategoryRepository();
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            base.OnActionExecuting(filterContext);
+
+            var categoryTree = _categoryRepository.GetCategoryTree();
+            filterContext.Controller.ViewBag.CategoryTree = categoryTree;
+        }
+    }
+}
--- PIO/Controllers/HomeController.cs
+++ PIO/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PIO.App_Start;
+using PIO.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,20 @@ namespace PIO.Controllers
 {
     public class HomeController : Controller
     {
+        private IQuestionRepository _questionRepository;
+
+        public HomeController()
+        {
+            _questionRepository = new QuestionRepository();
+        }
+        
+        [PassCategoryTree]
         public ActionResult Index()
         {
             return View();
         }
 
+        [PassCategoryTree]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +31,7 @@ namespace PIO.Controllers
             return View();
         }
 
+        [PassCategoryTree]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
