aedc26145704877314aeadef0986ce94ca9d51db
Jovan Jovanovski <jovanovskijovan97@gmail.com>
2020-01-26 01:14:30 +0100

    Added some unit tests

--- PIO.Tests/Controllers/HomeControllerTest.cs
+++ PIO.Tests/Controllers/HomeControllerTest.cs
@@ -6,12 +6,42 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PIO;
 using PIO.Controllers;
+using PIO.Services;
+using Moq;
+using PIO.Repositories;
 
 namespace PIO.Tests.Controllers
 {
     [TestClass]
     public class HomeControllerTest
     {
+        private Mock<IQuestionRepository> _questionRepositoryMock;
+        private Mock<IAnswerRepository> _answerRepositoryMock;
+        private Mock<ICategoryRepository> _categoryRepositoryMock;
+        private Mock<IUserRepository> _userRepositoryMock;
+
+        public HomeControllerTest()
+        {
+            _questionRepositoryMock = new Mock<IQuestionRepository>();
+            _answerRepositoryMock = new Mock<IAnswerRepository>();
+            _categoryRepositoryMock = new Mock<ICategoryRepository>();
+            _userRepositoryMock = new Mock<IUserRepository>();
+
+            Container.QuestionService = new QuestionService(_questionRepositoryMock.Object, _categoryRepositoryMock.Object, _userRepositoryMock.Object);
+        }
+
+        [DataRow(0, 1)]
+        [DataRow(1, 0)]
+        [DataRow(-1, -1)]
+        [DataTestMethod]
+        public void GetLatestQuestionsThrowsArgumentException(int page, int pageSize)
+        {
+            Assert.ThrowsException<ArgumentException>(() =>
+            {
+                Container.QuestionService.GetLatestQuestions(page, pageSize);
+            });
+        }
+
         [TestMethod]
         public void Index()
         {
